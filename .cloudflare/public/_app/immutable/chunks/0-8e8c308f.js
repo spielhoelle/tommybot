ae3024385478c88d791a99debfac31074c7dfa22
Tommy <spielhoelle@posteo.net>
2024-03-17 11:32:45 +0900

    chore: wipe build artifacts

--- .cloudflare/public/_app/immutable/chunks/0-8e8c308f.js
+++
@@ -1,4 +0,0 @@
-import { default as default2 } from "../components/pages/_layout.svelte-ebc491c9.js";
-export {
-  default2 as component
-};
--- .cloudflare/public/_app/immutable/chunks/2-3677231e.js
+++
@@ -1,4 +0,0 @@
-import { default as default2 } from "../components/pages/_page.svelte-a3e10c76.js";
-export {
-  default2 as component
-};
