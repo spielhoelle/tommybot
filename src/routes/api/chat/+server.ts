7b68e0d9610323ede9f2a23337daec358ceaaed0
Tommy <spielhoelle@posteo.net>
2024-06-02 18:27:12 +0900

    fix: try to add corsHeaders in BE

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -9,6 +9,11 @@ const PUBLIC_OLLAMA_BASE_URL = process.env.PUBLIC_OLLAMA_BASE_URL || 'https://ai
 // const exec = util.promisify(child_process.exec)
 
 export type MessageBody = { chats: { role: 'user' | 'assistant', content: string }[], model: string }
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*', // Specify the url you wish to permit
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
 
 const promptArray: string[] = []
 let promptTemplate = `Evaluate`
@@ -29,12 +34,12 @@ let promptTemplate = `Evaluate`
 //   const { stdout, stderr } = await promise
 //   if (stderr) {
 //     return new Response(JSON.stringify({ err: stderr }), {
-//       headers: { 'Content-Type': 'text/plain' },
+//       headers: corsHeaders,
 //     })
 //   }
 //   const res = stdout.split('\n').map(m => m.replace(':latest', ''))
 //   return new Response(JSON.stringify({ models: res }), {
-//     headers: { 'Content-Type': 'text/plain' },
+//     headers: corsHeaders,
 //   })
 // }
 export const POST = async ({ request }) => {
@@ -53,7 +58,7 @@ export const POST = async ({ request }) => {
   const roleToUseAndAbort = chatTasks.find(r => r.cmd === lastChatMessage)
   if (roleToUseAndAbort) {
     return new Response(roleToUseAndAbort.question, {
-      headers: { 'Content-Type': 'text/plain' },
+      headers: corsHeaders,
     })
   }
 
@@ -62,7 +67,7 @@ export const POST = async ({ request }) => {
     promptArray[taskToAbort] = lastChatMessage
     const nextAnswer = chatTasks[taskToAbort + 1]
     return new Response(nextAnswer ? nextAnswer.answer : 'About what?', {
-      headers: { 'Content-Type': 'text/plain' },
+      headers: corsHeaders,
     })
   }
   if (promptArray.length === 1) {
@@ -74,7 +79,7 @@ export const POST = async ({ request }) => {
   console.log('promptTemplate', promptTemplate, promptArray)
   // else {
   // return new Response('About what?', {
-  //   headers: { 'Content-Type': 'text/plain' },
+  //   headers: corsHeaders,
   // })
   // }
 
@@ -107,7 +112,7 @@ export const POST = async ({ request }) => {
   })
 
   return new Response(readableStream, {
-    headers: { 'Content-Type': 'text/plain' },
+    headers: corsHeaders,
   })
 }
 
